f9f403173acef05bb4aadea77262c958caf86c78
Vadim Tkachenko <vadim.tkach1378@gmail.com>
2026-04-26 09:57:45 +0300

    fixxed spin problem

--- src/components/ui/Hero3DWrapper.tsx
+++ src/components/ui/Hero3DWrapper.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import { Hero3DSpin } from '@/components/ui/Hero3DSpin'
 
 const Hero3DScene = dynamic(() => import('@/components/ui/Hero3DScene'), {
   ssr: false,
-  loading: () => <Hero3DSpin />,
+  loading: () => null,
 })
 
 export default function Hero3DWrapper() {
